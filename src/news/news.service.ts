589b97a395faf68559d5ba9fe61b36526d02399c
Alexis Gomez <gomezalexisj13@gmail.com>
2019-12-17 11:45:35 -0600

    :sparkles: Add function searchNews on NewsService
    Add function searchNews on NewsService

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  async searchNews(search: string, provider?: string): Promise<void> {
+    // let news: any[];
+    // switch (provider) {
+    //   case undefined:
+    //     guardianSearch(search);
+    //     nySearch(search);
+    //     break;
+    //   case 'guardian':
+    //     guardianSearch(search);
+    //     break;
+    //   case 'ny':
+    //     nySearch(search);
+    //     break;
+    //   default:
+    //   // Error unknown news provider.
+    // }
+    return;
+  }
+}
